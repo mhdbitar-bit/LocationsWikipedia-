a3cf2b157fed27c133a0118a058042e7ceb32113
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-03 19:40:12 +0300

    Add LocationEndPoint

---
+++ LocationWikipedia/LocationWikipedia/Network Layer/LocationEndpoint.swift
@@ -0,0 +1,49 @@
+//
+//  LocationEndpoint.swift
+//  LocationWikipedia
+//
+//  Created by Mohammad Bitar on 3/3/22.
+//
+
+import Foundation
+
+enum LocationEndpoint {
+    case getLocations
+}
+
+extension LocationEndpoint: Endpoint {
+    var shceme: String {
+        switch self {
+        default:
+            return "https"
+        }
+    }
+    
+    var baseURL: String {
+        switch self {
+        default:
+            return "raw.githubusercontent.com"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getLocations:
+            return "/abnamrocoesd/assignment-ios/main/locations.json"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        switch self {
+        case .getLocations:
+            return []
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .getLocations:
+            return "GET"
+        }
+    }
+}
