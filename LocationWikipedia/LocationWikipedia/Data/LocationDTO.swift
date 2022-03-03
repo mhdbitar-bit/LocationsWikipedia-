4643c8f87936906378ab3cfb70a751d5a16bbc2e
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-03 21:14:33 +0300

    Add Location Data to Object

---
+++ LocationWikipedia/LocationWikipedia/Data/LocationDTO.swift
@@ -0,0 +1,14 @@
+//
+//  LocationDTO.swift
+//  LocationWikipedia
+//
+//  Created by Mohammad Bitar on 3/3/22.
+//
+
+import Foundation
+
+struct LocationDTO: Equatable {
+    let name: String
+    let latitude: Double
+    let longitude: Double
+}
