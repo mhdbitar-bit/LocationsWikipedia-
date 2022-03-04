448bf402953c78a8c7f29e7428b7801f979e20e1
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-04 21:11:25 +0300

    Build mapper and data model

--- LocationWikipedia/LocationWikipedia/Data/LocationModels.swift
+++ LocationWikipedia/LocationWikipedia/Data/LocationModels.swift
@@ -7,12 +7,32 @@
 
 import Foundation
 
-struct LocationResponse: Codable {
-    let locations: [Location]
+public final class LocationMapper: Decodable {
+    private struct LocationResponse: Decodable {
+        private let locations: [LocationItem]
+        
+        private struct LocationItem: Decodable {
+            let name: String
+            let lat: Double
+            let long: Double
+        }
+        
+        var items: [LocationDTO] {
+            locations.map { LocationDTO(name: $0.name, latitude: $0.lat, longitude: $0.long) }
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [LocationDTO] {
+        guard response.statusCode == 200, let locations = try? JSONDecoder().decode(LocationResponse.self, from: data) else {
+            throw Error.invalidData
+        }
+        
+        return locations.items
+    }
 }
 
-struct Location: Codable {
-    let name: String
-    let lat: Double
-    let long: Double
-}
+
---
+++ LocationWikipedia/LocationWikipedia/LocationsViiewController.swift
@@ -0,0 +1,14 @@
+//
+//  LocationsViiewController.swift
+//  LocationWikipedia
+//
+//  Created by Mohammad Bitar on 3/3/22.
+//
+
+import UIKit
+
+final class LocationsViiewController: UITableViewController {
+    
+    
+    
+}
