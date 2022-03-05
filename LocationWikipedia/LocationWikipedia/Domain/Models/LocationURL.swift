d74619b25ddb661b0003a75590e057309f28300d
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-05 12:35:22 +0300

    Add `LocationURL` model

---
+++ LocationWikipedia/LocationWikipedia/Domain/Models/LocationURL.swift
@@ -0,0 +1,16 @@
+//
+//  LocationURL.swift
+//  LocationWikipedia
+//
+//  Created by Mohammad Bitar on 3/5/22.
+//
+
+import Foundation
+
+public struct LocationURL {
+    public static let scheme = "wikipedia"
+    public static let host = "places"
+    public static let longitude = "longitude"
+    public static let latitude = "latitude"
+    public static let place = "place"
+}
