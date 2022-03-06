1e65f48d2107223529ff4a3f73dc66acfe3977d4
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-06 20:48:48 +0300

    Move URLResponse extension to the Shared file + Add two more helper functions

--- LocationWikipedia/LocationWikipediaTests/Helpers/SharedTestHelpers.swift
+++ LocationWikipedia/LocationWikipediaTests/Helpers/SharedTestHelpers.swift
@@ -6,7 +6,47 @@
 //
 
 import Foundation
+import LocationWikipedia
 
 func anyURL() -> URL {
     return URL(string: "https://any-url.com")!
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "", code: 0)
+}
+
+func anyResponse() -> HTTPURLResponse {
+    return HTTPURLResponse(statusCode: 200)
+}
+
+func makeJson(_ locations: [[String: Any]]) -> Data {
+    let json = ["locations": locations]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+func makelocation(name: String, lat: Double, long: Double) -> (model: Location, json: [String: Any]) {
+    let location = Location(name: name, coordinators: (latitude: lat, longitude: long))
+    
+    let json = [
+        "name": name,
+        "lat": lat,
+        "long": long
+    ].compactMapValues { $0 }
+    
+    return (location, json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(
+            url: anyURL(),
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil)!
+    }
 }
--- LocationWikipedia/LocationWikipediaTests/LocationMapperTests.swift
+++ LocationWikipedia/LocationWikipediaTests/LocationMapperTests.swift
@@ -50,33 +50,4 @@ class LocationMapperTests: XCTestCase {
         XCTAssertEqual(result, [location1.model, location2.model])
         
     }
-    
-    // MARK: - Helpers
-    
-    private func makeJson(_ locations: [[String: Any]]) -> Data {
-        let json = ["locations": locations]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-    
-    private func makelocation(name: String, lat: Double, long: Double) -> (model: Location, json: [String: Any]) {
-        let location = Location(name: name, coordinators: (latitude: lat, longitude: long))
-        
-        let json = [
-            "name": name,
-            "lat": lat,
-            "long": long
-        ].compactMapValues { $0 }
-        
-        return (location, json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(
-            url: anyURL(),
-            statusCode: statusCode,
-            httpVersion: nil,
-            headerFields: nil)!
-    }
 }
