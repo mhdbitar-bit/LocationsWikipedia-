22bafba8e661262a489562f7464e8d1714825c58
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-07 18:15:17 +0300

    Build `AddNewLocationViewController`

---
+++ LocationWikipedia/LocationWikipedia/Presentation/AddLocationScene/Views/AddLocationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddLocationViewController.swift
+//  LocationWikipedia
+//
+//  Created by Mohammad Bitar on 3/7/22.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Add Location"
+    }
+
+}
