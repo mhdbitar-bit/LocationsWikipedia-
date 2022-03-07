4d63c77e38ba201037be91fc9daeff962d9c4f71
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-03-07 20:59:21 +0300

    add configureWindow function

--- LocationWikipedia/LocationWikipedia/SceneDelegate.swift
+++ LocationWikipedia/LocationWikipedia/SceneDelegate.swift
@@ -6,20 +6,24 @@
 //
 
 import UIKit
-import Combine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     private let navigationController = UINavigationController()
-   
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
         navigationController.setViewControllers([makeRootViewController()], animated: false)
+        
+        configureWindow()
+    }
+    
+    func configureWindow() {
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
